4914a383886da61270a1f9265a5ea4922fea06f7
Julien Michon <mcn.julien@gmail.com>
2023-06-27 14:40:22 +0200

    Exercice 001 finished

--- 01-bases/major.php
+++ 01-bases/major.php
@@ -5,6 +5,33 @@
     <title>Exercices 001</title>
 </head>
 <body>
-    
+    <?php
+        $age = 18;
+        $allowed;
+        $major = "";
+
+        if ($age >= 18) {
+            $allowed = true;
+        } else {
+            $allowed = false;
+        }
+
+        if ($age < 18 && $age >= 16) {
+            $major = "Vous êtes presque majeur";
+        } else if ($age >= 14 && $age < 16) {
+            $major = "Vous êtes jeune.";
+        } else if ($age < 14) {
+            $major = "Vous êtes trop jeune";
+        } else {
+            $major = "Vous êtes majeur";
+        }
+    ?>
+
+    <h1><?= $major ?></h1>
+
+    <p>Vous avez <?= $age; ?> ans.</p>
+
+    <p><?= $allowed == true ? "Vous pouvez entrer" : "L'entrée vous est interdite"; ?> </p>
+
 </body>
 </html>
