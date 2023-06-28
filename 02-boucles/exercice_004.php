96c8e7a0ebd73584c518890ec87e9636380aecd5
Julien Michon <mcn.julien@gmail.com>
2023-06-28 12:00:07 +0200

    exo4 fix

--- 02-boucles/exercice_004.php
+++ 02-boucles/exercice_004.php
@@ -9,18 +9,19 @@
 
 <body>
     <main>
-        <div class="x">X</div>
-        <div class="horizontal">
+        <div class="x bold">x</div>
+        <div class="horizontal bold">
             <?php for ($i = 0; $i <= 10; $i++) { ?>
                 <span><?= $i; ?></span>
             <?php } ?>
         </div>
-        <div class="vertical">
+        <div class="vertical bold ">
             <?php for ($i = 0; $i <= 10; $i++) { ?>
                 <span><?= $i; ?></span><br>
             <?php } ?>
         </div>
         <div class="content">
+            <?php $class = '' ?>
             <?php for ($i = 0; $i <= 10; $i++) { ?>
                 <span class="column"><?= 0; ?></span>
                 <?php for ($j = 1; $j <= 10; $j++) { ?>
