b7ee6c81c0e41da50be20fb00d10a83ef05dc4aa
Julien Michon <mcn.julien@gmail.com>
2023-07-10 15:03:34 +0200

    cours add class

--- 12-poo/index.php
+++ 12-poo/index.php
@@ -4,5 +4,20 @@ require 'vendor/autoload.php';
 
 use App\Cat;
 
-$bianca = new Cat();
-var_dump($bianca);
+$bianca = new Cat('Bianca');
+$bianca->setFur('blanc')->setFur('noir');
+$mina = new Cat('Mina');
+
+dump($bianca, $mina);
+
+?>
+
+<h1>Mon premier chat s'apelle <?= $bianca->getName(); ?></h1>
+<p>Et <?= $bianca->cry(); ?></p>
+<p>
+    L'autre chat est <?= $mina->getName(); ?>.
+    <br>
+    Et <?= $mina->cry(); ?>
+</p>
+
+<p><?= $bianca->playWith($mina); ?></p>
--- 12-poo/vendor/composer/autoload_real.php
+++ 12-poo/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit5832476e162d6cc63ba26b55125f78c5
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit5832476e162d6cc63ba26b55125f78c5', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit5832476e162d6cc63ba26b55125f78c5', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit5832476e162d6cc63ba26b55125f78c5
         call_user_func(\Composer\Autoload\ComposerStaticInit5832476e162d6cc63ba26b55125f78c5::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit5832476e162d6cc63ba26b55125f78c5::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
